bdcff81bac226ea6c4712a322652c8bf90abc9b4
Pavel Larin <www.larinpavel@mail.ru>
2023-12-01 01:40:22 +0500

    Changed to port 8282 in application.yml and, in the test folder, created resources/application.properties

--- src/main/java/com/example/demo/HelloWorldController.java
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -9,7 +9,7 @@ public class HelloWorldController {
     @GetMapping(path = "/hello")
     public String helloWorld() {
         return "Hello World!";
-        // http://localhost:8080/hello // 8181 after server.port=8181, or it can be in application.yml
+        // http://localhost:8080/hello // 8181 after server.port=8181, or it can be in application.yml, server: port:8282
     }
 
 }

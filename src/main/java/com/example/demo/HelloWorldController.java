ddc2dd938b4aa6a3baec36da37b2de91faf384a9
Pavel Larin <www.larinpavel@mail.ru>
2023-12-01 01:36:09 +0500

    Changed to port 8181 in application.properties

--- src/main/java/com/example/demo/HelloWorldController.java
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -9,7 +9,7 @@ public class HelloWorldController {
     @GetMapping(path = "/hello")
     public String helloWorld() {
         return "Hello World!";
-        // http://localhost:8080/hello
+        // http://localhost:8080/hello // 8181 after server.port=8181, or it can be in application.yml
     }
 
 }
